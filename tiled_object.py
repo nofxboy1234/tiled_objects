33f533fc8f1b19e97acbab0ee70cc4c2ff534899
Dylan Palmboom <nofxboy1234@gmail.com>
2015-07-14 22:36:36 +0200

    Refactored code using python class for tiled_objects.

--- tiled_object.py
+++ tiled_object.py
@@ -1,3 +1,6 @@
+# TODO: Set room settings and view
+#       Add player object with code, toggle visibility
+
 from xml.etree.ElementTree import Element, SubElement, parse, ElementTree, XMLParser
 from xml.etree.ElementTree import tostring, fromstring
 from xml.dom import minidom
@@ -8,28 +11,60 @@ class Tiled_ObjectGroup(object):
         self.sprite = "spr_%s" % (group_name)
 
 class Tiled_Object(Tiled_ObjectGroup):
-    def __init__(self, group_name, tiled_id, x, y, width, height,
+    def __init__(self, group_name, tiled_id="", x="0", y="0",
+                        width="32", height="32",
                         object_name="", tiled_type="",
                         sprite=""):
         super(Tiled_Object, self).__init__(group_name)
 
+        self.name = object_name
         self.id = tiled_id
         self.x = x
         self.y = y
         self.width = width
         self.height = height
-        self.object_name = object_name
         self.type = tiled_type
 
         if sprite != "":
             self.sprite = sprite
+
+    def __str__(self):
+        print_str = "group_name: %s\n" \
+                    "name: %s\n" \
+                    "id: %s\n" \
+                    "x: %s\n" \
+                    "y: %s\n" \
+                    "width: %s\n" \
+                    "height: %s\n" \
+                    "type: %s\n" \
+                    "sprite: %s\n" \
+                    % (
+                        self.group_name,
+                        self.name,
+                        self.id,
+                        self.x,
+                        self.y,
+                        self.width,
+                        self.height,
+                        self.type,
+                        self.sprite
+                        )
+        return print_str
 
 def prettify(elem):
     rough_string = tostring(elem, 'utf-8')
     reparsed = minidom.parseString(rough_string)
     return reparsed.toprettyxml(indent="  ")
 
-def create_GMS_sprite(name, width, height, output_dir, sprite):
+def create_GMS_sprite(tiled_object, output_dir):
+    name = tiled_object.name
+    if name == "":
+        name = tiled_object.group_name
+
+    width = tiled_object.width
+    height = tiled_object.height
+    sprite = tiled_object.sprite
+
     root = Element("sprite")
 
     elem = SubElement(root, "type")
@@ -118,15 +153,34 @@ def get_tiled_objects(filename):
         objects = []
         for tiled_object in tiled_object_list:
             tiled_object_dict = {}
+            to = Tiled_Object(tiled_objectgroup.get('name'))
 
-            tiled_object_dict['name'] = tiled_objectgroup.get('name')
-            tiled_object_dict['type'] = tiled_objectgroup.get('name')
+            # tiled_object_dict['id'] = tiled_object.get('id')
+            to.id = tiled_object.get('id')
 
-            tiled_object_dict['id'] = tiled_object.get('id')
-            tiled_object_dict['x'] = tiled_object.get('x')
-            tiled_object_dict['y'] = tiled_object.get('y')
-            tiled_object_dict['width'] = tiled_object.get('width')
-            tiled_object_dict['height'] = tiled_object.get('height')
+            # tiled_object_dict['x'] = tiled_object.get('x')
+            to.x = tiled_object.get('x')
+
+            # tiled_object_dict['y'] = tiled_object.get('y')
+            to.y = tiled_object.get('y')
+
+            # tiled_object_dict['width'] = tiled_object.get('width')
+            to.width = tiled_object.get('width')
+
+            # tiled_object_dict['height'] = tiled_object.get('height')
+            to.height = tiled_object.get('height')
+
+            # tiled_object_dict['name'] = tiled_objectgroup.get('name')
+            # to.object_name = tiled_object.get('name')
+            to.name = tiled_object.get('name')
+            if to.name is None:
+                to.name = ""
+
+            # tiled_object_dict['type'] = tiled_objectgroup.get('name')
+            # to.tiled_type = tiled_object.get('type')
+            to.type = tiled_object.get('type')
+            if to.type is None:
+                to.type = ""
 
             # Check if there's a sprite set for the object
             # This will override any objectgroup sprite set
@@ -142,18 +196,26 @@ def get_tiled_objects(filename):
 
             # If there's an object_sprite, use this instead of objectgroup_sprite
             if object_sprite != '':
-                tiled_object_dict['sprite'] = object_sprite
+                # tiled_object_dict['sprite'] = object_sprite
+                to.sprite = object_sprite
             else:
-                tiled_object_dict['sprite'] = objectgroup_sprite
+                # tiled_object_dict['sprite'] = objectgroup_sprite
+                to.sprite = objectgroup_sprite
 
-            objects.append(tiled_object_dict)
+            # objects.append(tiled_object_dict)
+            objects.append(to)
+
         # Add a group to the groups dict
         # groups = {group_name: [{'name': '',},]}
         groups[tiled_objectgroup.get('name')] = objects
 
     return groups
 
-def create_GMS_object(name, output_dir):
+def create_GMS_object(tiled_object, output_dir):
+    name = tiled_object.name
+    if name == "":
+        name = tiled_object.group_name
+
     root = Element("object")
 
     elem = SubElement(root, "spriteName")
@@ -287,23 +349,28 @@ def add_instance_to_room(tiled_object, room_filename, tmx_filename):
 
     existing_instances_element = root.find('instances')
     if existing_instances_element is None:
-        print("1")
+        # print("1")
         instances = Element("instances")
     else:
-        print("2")
+        # print("2")
         instances = existing_instances_element
 
     room_name = room_filename.split('/')[-1].split(".")[0]
 
+    if tiled_object.name != "":
+        object_room_name = tiled_object.name
+    else:
+        object_room_name = tiled_object.group_name
+
     elem = Element("instance",
-                        objName = "obj_%s" % (tiled_object['name']),
-                        x = tiled_object['x'],
-                        y = tiled_object['y'],
-                        name = "inst_%s_%04d" % (room_name, int(tiled_object['id'])),
+                        objName = "obj_%s" % (object_room_name),
+                        x = tiled_object.x,
+                        y = tiled_object.y,
+                        name = "inst_%s_%04d" % (room_name, int(tiled_object.id)),
                         locked = "0",
                         code = "",
-                        scaleX = str(int(tiled_object['width'])/int(tilewidth)),
-                        scaleY = str(int(tiled_object['height'])/int(tileheight)),
+                        scaleX = str(int(tiled_object.width)/int(tilewidth)),
+                        scaleY = str(int(tiled_object.height)/int(tileheight)),
                         colour = "4294967295",
                         rotation = "0")
     instances.append(elem)
@@ -325,17 +392,24 @@ def main():
     for group in object_groups:
         for tiled_object in object_groups[group]:
             print(str(tiled_object))
-            create_GMS_sprite(tiled_object['name'],
-                                tiled_object['width'],
-                                tiled_object['height'],
-                                "C:/Users/dylan/tiled_maps",
-                                tiled_object['sprite'])
+            # create_GMS_sprite(tiled_object['name'],
+            #                     tiled_object['width'],
+            #                     tiled_object['height'],
+            #                     "C:/Users/dylan/tiled_maps",
+            #                     tiled_object['sprite'])
+            create_GMS_sprite(tiled_object,
+                    "C:/Users/dylan/tiled_maps")
 
-            create_GMS_object(tiled_object['name'],
+            # create_GMS_object(tiled_object['name'],
+            #                     "C:/Users/dylan/tiled_maps")
+            create_GMS_object(tiled_object,
                                 "C:/Users/dylan/tiled_maps")
 
+            # add_instance_to_room(tiled_object,
+            #                     "C:/Users/dylan/tiled_maps/crate_land/crate_land.room.gmx",
+            #                     "C:/Users/dylan/tiled_maps/crate_land.tmx")
             add_instance_to_room(tiled_object,
                                 "C:/Users/dylan/tiled_maps/crate_land/crate_land.room.gmx",
                                 "C:/Users/dylan/tiled_maps/crate_land.tmx")
 
-# main()
+main()
